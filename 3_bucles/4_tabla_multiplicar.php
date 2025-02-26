8958f55aef5bff59c759155dfd7ccafdbd4a66c5
L-edu1723 <l.edu31espinosa@gmail.com>
2025-02-26 18:26:09 +0100

    bucles

--- 3_bucles/4_tabla_multiplicar.php
+++ 3_bucles/4_tabla_multiplicar.php
@@ -1,10 +1,9 @@
 <?php
-$x=7;
 for ($i = 0; $i < 10; $i++) {
     $resultado=7*$i;
     echo "<table border='1'>
         <tr>
-            <td>$x*$i=$resultado</td>
+            <td>7*$i=$resultado</td>
         </tr>";
 
 
