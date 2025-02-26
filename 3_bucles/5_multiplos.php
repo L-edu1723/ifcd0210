88a1f1ba6078ff177f2abb39e37829c01677c920
L-edu1723 <l.edu31espinosa@gmail.com>
2025-02-26 19:06:02 +0100

    bucles

--- 3_bucles/5_multiplos.php
+++ 3_bucles/5_multiplos.php
@@ -1,10 +1,11 @@
 <?php
+    echo "<table border='1'>";
 
 for ($i = 1; $i <=300; $i++) {
     $multiplo=3*$i;
-    echo "<table border='1'>
-    <tr>
+    echo "<tr>
         <td>3*$i=$multiplo</td>
     </tr>";
 
 }
+echo "</table>";
